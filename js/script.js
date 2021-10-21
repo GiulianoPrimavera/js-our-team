fbccd390b36f946de85f6ff2ab527716e6537d45
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-10-21 15:56:14 +0200

    visualize in the browser console every key value of every object in the array

--- js/script.js
+++ js/script.js
@@ -10,32 +10,32 @@
 //creo l'arrei e lo popolo di oggetto (il team)
 const ourTeamArray = [
     {
-        name: "Wayne Barnett",
+        nome: "Wayne Barnett",
         role: "Founder & CEO",
         picture: "wayne-barnett-founder-ceo.jpg"
     },
     {
-        name: "Angela Carol",
+        nome: "Angela Carol",
         role: "Chief Editor",
         picture: "angela-carol-chief-editor.jpg"
     },
     {
-        name: "Walter Gordon",
+        nome: "Walter Gordon",
         role: "Office Manager",
         picture: "walter-gordon-office-manager.jpg"
     },
     {
-        name: "Angela Lopez",
+        nome: "Angela Lopez",
         role: "Social Media Manager",
         picture: "angela-lopez-social-media-manager.jpg"
     },
     {
-        name: "Scott Estrada",
+        nome: "Scott Estrada",
         role: "Developer",
         picture: "scott-estrada-developer.jpg"
     },
     {
-        name: "Barbara Ramos",
+        nome: "Barbara Ramos",
         role: "Graphic Designer",
         picture: "barbara-ramos-graphic-designer.jpg"
     },
@@ -44,12 +44,12 @@ const ourTeamArray = [
 const teamContainer = document.querySelector(".team-container");
 
 function teamCardsGenerator(){
-    //devo creare un ciclo che per ogni oggetto nell'array del team genera un html con i parametri giusti
+    //ciclo che "scorre" ogni elemento dell'array e lo associa 
     for (let i = 0; i < ourTeamArray.length; i++ ){
         const singleMember = ourTeamArray[i];
         console.log("il membro del team", singleMember);
         for (let j in singleMember){
-            
+             console.log(singleMember[j]);
         }
     }
     

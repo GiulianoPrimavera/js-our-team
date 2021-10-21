9e1d6a75690a5eed795314ceeaa617ff28bb71a7
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-10-21 15:35:23 +0200

    adds array of object

--- js/script.js
+++ js/script.js
@@ -6,3 +6,37 @@
 -utiliziamo gli input già presenti nell'html per inserire nuovi membri nel team
 
 */
+
+//creo l'arrei e lo popolo di oggetto (il team)
+const ourTeamArray = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        picture: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela Carol",
+        role: "Chief Editor",
+        picture: "angela-carol-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        picture: "walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        picture: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        picture: "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        picture: "barbara-ramos-graphic-designer.jpg"
+    },
+]

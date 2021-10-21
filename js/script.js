6bf59376032d7efd647fe563ff619f948a854875
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-10-21 15:28:05 +0200

    adds script.js file with todo comments

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*  //todo
+-creo l'array di oggetti che rappresentano ciascun membro del team
+    -ogni membro deve avere : Nome completo, ruolo e foto (questo è l'oggetto che deve andare nell'array di oggetti)
+-stampo tutte le card del nostro team nel team-container
+    
+-utiliziamo gli input già presenti nell'html per inserire nuovi membri nel team
+
+*/
